0e19920910c128c8ce8246dfbd914c2fa611e581
Leo Motovskikh <multicoloursocks@gmail.com>
2013-09-26 23:59:48 +0400

    removed some bad data

--- data.py
+++ data.py
@@ -1,31 +1,30 @@
-# check if there's no need in extra vars!!
-# squiggle texture is the worst, need to be fixed.
 import os
 import pygame as pg
 from pygame.locals import *
 
 # basic and engine things. # q: quantity, c: colour, f: fill, s: shape
-quantity = ('1', '2', '3')  # or 0, 1, 2?
+quantity = ('1', '2', '3')
 colours  = ('Red', 'Green', 'Purple')
 fill     = ('empty', 'striped', 'filled')
 shapes   = ('oval', 'squiggle', 'diamond')
 
-# thigs for pygame:
+display = (800, 600)
 name = "Set"
 filepath = os.path.dirname(__file__)
-display = window_width, window_height = 800, 600
+#png = '.png'   # do I really need this thing?
 
-# card has lots of attributes, so it's better to store them in dict
 card_attr = {
     'width': 106, 'height': 160,
     'X': 170, 'Y': 15, 'space': 10
 }
 empty_card = pg.image.load('{}/cards/card.png'.format(filepath))
+selection = pg.image.load('{}/cards/selection.png'.format(filepath))
 
+# these are added to x and y so the right position is chosen.
 symbol_width, symbol_height = 80, 42  # 42!!
 add_x = (card_attr['width'] - symbol_width)/2
 add_space = 4
-add_y = {  # unite three and one? # hope, you'll see where the values come from
+add_y = {
     '1': [(card_attr['height'] - symbol_height)/2],
     '2': [(card_attr['height'] - 2*symbol_height - add_space)/2, 
           (card_attr['height'] + add_space)/2],
@@ -36,13 +35,9 @@ add_y = {  # unite three and one? # hope, you'll see where the values come from
 
 # Green, Purple and Red do NOT look as they are named. Don't ask why
 palette = {
-    'Black':        (  0,   0,   0),    'White':        (255, 255, 255),
-    'Yellow':       (255, 255,   0),    'Light Yellow': (250, 255, 150),
-    'Red':          (255,   0, 120),    'Purple':       (120, 120, 210),
-    'Green':        ( 55, 205,  65)
+    'Light Grey':   (200, 200, 200),    'Green':        ( 55, 205,  65),
+    'Red':          (255,   0, 120),    'Purple':       (120, 120, 210)
 }
-
-#png = '.png'   # do I really need this thing?
 
 class Card(pg.sprite.Sprite):
 
@@ -50,23 +45,24 @@ class Card(pg.sprite.Sprite):
         self.attr = (
             self.quantity, self.colour, self.fill, self.shape) = (
             q, c, f, s)
-        pg.sprite.Sprite.__init__(self)
+        self.selected = False
 
     def draw(self, x, y):
-        # the symbol(s) = filled coloured rectangle + alpha-mask over
+        #pg.sprite.Sprite.__init__(self)
+        self.coord = (x, y)
+        self.rect = pg.Rect(self.coord,
+            (card_attr['width'], card_attr['height']))
         screen = pg.display.get_surface()
-        innername = '{}/cards/{}{}.png'.format(
+        screen.blit(empty_card, self.coord)
+
+        # the symbol(s) == filled coloured rectangle + alpha-mask over
+        symbol_name = '{}/cards/{}{}.png'.format(
             filepath, self.fill[0], self.shape[0])
-        innerimg = pg.image.load(innername)
+        symbol_img = pg.image.load(symbol_name)
         for i in xrange(int(self.quantity)):
             pg.draw.rect(screen, palette[self.colour],
                 [x+add_x, y+add_y[self.quantity][i],
                 symbol_width, symbol_height])
-            screen.blit(innerimg,
+            screen.blit(symbol_img,
                 (x+add_x, y+add_y[self.quantity][i]))
-
-    ''' MAYBE there will be need in it
-    def __str__(self):
-        s = ''
-        if self.quantity > '1': s = 's'
-        return '{} {} {} {}'.format(*self.attr) + s'''
+        if self.selected: screen.blit(selection, (x-2, y-2))
